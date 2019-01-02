030d92500ce519e7d99ed1eecc442c6855041e2e
yijigu7745 <774598105@qq.com>
2019-01-02 16:43:15 +0800

    cjh test

--- app/src/main/java/com/ghhitech/smisseal/constants/SealConstants.java
+++ app/src/main/java/com/ghhitech/smisseal/constants/SealConstants.java
@@ -4,6 +4,7 @@ public class SealConstants {
 
     public static final String BASE_URL = "http://192.168.1.164:8081/blasting-app/api/";
 
+
     public static final int DEFAULT_TIMEOUT = 5;
 
     public static final int MINUTES_TIMEOUT = 60;
